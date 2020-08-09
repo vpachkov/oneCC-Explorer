f2c8f976a1dd42f986d087a6f94859a36a789db9
Plunkerusr <slava.pach@gmail.com>
2020-08-09 23:01:26 +0300

    Router: add react router

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { PMain } from "./pages/PMain"
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from 'react-router-dom'
+
+import { PMain } from './pages/PMain'
+
 import 'semantic-ui-css/semantic.min.css'
 import './css/App.css'
 import './css/fonts.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <PMain/>
+      <Router>
+          <Switch>
+              <Route path="/">
+                  <PMain/>
+              </Route>
+          </Switch>
+      </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
