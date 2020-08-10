b3e1452672b703ebb5969bff29cd467fbe88d56e
nimelehin <nimelehin@gmail.com>
2020-08-10 10:56:10 +0300

    Explore: Make blocks vertical

--- ui/src/pages/PExplore.tsx
+++ ui/src/pages/PExplore.tsx
@@ -33,23 +33,23 @@ export class PExplore extends PBase<P, S> {
                 {/*</div>*/}
                 <div style={{ marginTop: '44px' }}>
                     <div className="labelC disabled xl">SOURCES</div>
-                    <div style={{ display: 'flex', marginTop: '32px'}}>
+                    <div className="block vertical">
                         <Link text="oneCC"/>
-                        <div style={{ marginLeft: '16px' }}>
+                        <div style={{ marginTop: '16px' }}>
                         <Link text="oneCC-Explorer"/>
                         </div>
                     </div>
                 </div>
                 <div style={{ marginTop: '44px' }}>
                     <div className="labelC disabled xl">AUTHORS</div>
-                    <div style={{ display: 'flex', marginTop: '32px'}}>
+                    <div className="block vertical">
                         <div>
                         <Link text="Vyacheslav Pachkov" additionalText="aka Plunkerusr" url="https://github.com/Plunkerusr"/>
                         </div>
-                        <div style={{ marginLeft: '16px' }}>
+                        <div style={{ marginTop: '16px' }}>
                             <Link text="Nikita Melokhin" additionalText="aka nimelehin" url="https://github.com/nimelehin"/>
                         </div>
-                        <div style={{ marginLeft: '16px' }}>
+                        <div style={{ marginTop: '16px' }}>
                             <Link text="Arman Djilavan" additionalText="aka armannz" url="https://github.com/armannz"/>
                         </div>
                     </div>
