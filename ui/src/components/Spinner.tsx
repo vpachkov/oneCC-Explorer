ad352392725df800578f6502732f51ba8758c5d6
nimelehin <nimelehin@gmail.com>
2020-08-10 09:11:23 +0300

    UI: Add label to spinner

--- ui/src/components/Spinner.tsx
+++ ui/src/components/Spinner.tsx
@@ -8,8 +8,11 @@ interface S {}
 export class Spinner extends Component<P, S> {
     render() {
         return (
-            <div className="loading-icon-container">
-                <div className="loading-icon"/>
+            <div className="loading-container">
+                <div className="loading-icon-container">
+                    <div className="loading-icon"/>
+                </div>
+                <div className="loading-label">Compiling</div>
             </div>
         )
     }
